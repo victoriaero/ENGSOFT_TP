d8005ca48f8173966391de56011f895cc1537503
Caio Santana <127700730+caio-santt@users.noreply.github.com>
2025-05-12 13:01:20 -0300

    Delete src directory

--- src/connect.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-class Database:
-    def __init__(self, db_path="db/data_labeler.db"):
-        self.db_path = db_path
-
-    def connect(self):
-        return sqlite3.connect(self.db_path)
